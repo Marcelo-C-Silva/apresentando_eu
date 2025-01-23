5ec616014676d5e9c1b1435361d5c6b662765f3b
marcelo <carneiromarcelodev@gmail.com>
2025-01-23 11:42:27 -0300

    feat: atualizar modal para suportar múltiplas imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const modalTitle = projectModal.querySelector('.modal-title');
   const modalBody = projectModal.querySelector('.modal-body');
   const projectLink = document.getElementById('projectLink');
-  const modalImage = projectModal.querySelector('.modal-image'); 
-
+  const modalImagesContainer = projectModal.querySelector('.window-pane span');
 
   const imagePath = 'assets/img/icons/';
 
   projectModal.addEventListener('show.bs.modal', (event) => {
-    const button = event.relatedTarget; 
+    const button = event.relatedTarget;
     const title = button.getAttribute('data-title');
     const description = button.getAttribute('data-description');
     const link = button.getAttribute('data-link');
-    const imageName = button.getAttribute('data-image');
+    const images = button.getAttribute('data-image').split(' ');
 
     console.log('Título:', title);
     console.log('Descrição:', description);
     console.log('Link:', link);
-    console.log('Imagem:', imageName);
-
+    console.log('Imagens:', images);
 
     modalTitle.textContent = title;
     modalBody.textContent = description;
     projectLink.href = link;
-    
-    
-    if (imageName) {
-      modalImage.src = imagePath + imageName; 
-      modalImage.style.display = 'block';
-      console.log("Imagem carregada:", modalImage.src);
-    } else {
-      modalImage.style.display = 'none';
+
+    // Limpa o container de imagens
+    modalImagesContainer.innerHTML = '';
+
+    // Adiciona cada imagem
+    if (images) {
+      images.forEach(imageName => {
+        if (imageName) {
+          const img = document.createElement('img');
+          img.src = imagePath + imageName;
+          img.className = 'tag-icons modal-image img-fluid mb-3 me-2';
+          img.alt = 'Ícone de tecnologia';
+          modalImagesContainer.appendChild(img);
+        }
+      });
     }
   });
 });
